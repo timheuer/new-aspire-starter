06c1a0ede89642d615302635348bfb5d6e88a7a1
Tim Heuer <tim@timheuer.com>
2023-12-02 11:05:57 -0800

    added resources

--- new-aspire-starter.AppHost/Program.cs
+++ new-aspire-starter.AppHost/Program.cs
@@ -1,9 +1,20 @@
+using Aspire.Hosting.Azure;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiservice = builder.AddProject<Projects.new_aspire_starter_ApiService>("apiservice");
+// dependencies
+var frontendcache = builder.AddRedisContainer("frontendcache");
+var db = builder.AddPostgresContainer("db");
+var photostorage = builder.AddAzureStorage("photostorage");
+var blobgs = photostorage.AddBlobs("photos");
+
+// microservices
+var apiservice = builder.AddProject<Projects.new_aspire_starter_ApiService>("apiservice")
+    .WithReference(db);
 
 builder.AddProject<Projects.new_aspire_starter_Web>("webfrontend")
-    .WithReference(apiservice);
-
+    .WithReference(apiservice)
+    .WithReference(frontendcache)
+    .WithReference(blobgs);
 
 builder.Build().Run();
