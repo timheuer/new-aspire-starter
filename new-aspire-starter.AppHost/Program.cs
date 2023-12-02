70085a2c8a27e6feddc80e13ff3d98931c839004
Tim Heuer <tim@timheuer.com>
2023-12-02 11:09:04 -0800

    remove postgres

--- new-aspire-starter.AppHost/Program.cs
+++ new-aspire-starter.AppHost/Program.cs
@@ -4,13 +4,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // dependencies
 var frontendcache = builder.AddRedisContainer("frontendcache");
-var db = builder.AddPostgresContainer("db");
+
 var photostorage = builder.AddAzureStorage("photostorage");
 var blobgs = photostorage.AddBlobs("photos");
 
 // microservices
-var apiservice = builder.AddProject<Projects.new_aspire_starter_ApiService>("apiservice")
-    .WithReference(db);
+var apiservice = builder.AddProject<Projects.new_aspire_starter_ApiService>("apiservice");
 
 builder.AddProject<Projects.new_aspire_starter_Web>("webfrontend")
     .WithReference(apiservice)
