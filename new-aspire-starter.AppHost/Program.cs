49866f503b130dae0fedfe55b71e8d57a8dbcd3e
Tim Heuer <tim@timheuer.com>
2023-12-09 07:56:23 -0800

    Update Program.cs

--- new-aspire-starter.AppHost/Program.cs
+++ new-aspire-starter.AppHost/Program.cs
@@ -8,7 +8,7 @@ var frontendcache = builder.AddRedisContainer("frontendcache");
 var photostorage = builder.AddAzureStorage("psstorage");
 var blobgs = photostorage.AddBlobs("photos");
 var keys = builder.AddAzureKeyVault("keys");
-var db = builder.AddPostgresContainer("db");
+var db = builder.AddPostgresContainer("db").AddDatabase("psdb");
 
 // microservices
 var apiservice = builder.AddProject<Projects.new_aspire_starter_ApiService>("apiservice")
